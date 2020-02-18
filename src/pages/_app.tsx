acdf16f602cd05ca211c5a6218afa36d62c14509
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-02-18 12:28:34 +0100

    optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import NextApp, { AppContext } from 'next/app';
+import NextApp from 'next/app';
 import Head from 'next/head';
 import ReactGA from 'react-ga';
 import withRedux from 'next-redux-wrapper';
 import { Store } from 'redux';
 import { Provider } from 'react-redux';
-import { toast, Flip } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Navbar from '../components/Navbar';
 import { initStore } from '../store';
 import '../types';
-
-import Navbar from '../components/Navbar';
 
 import './_app.scss';
 
 toast.configure({
-	autoClose: 3000,
+	autoClose: 5000,
 	pauseOnHover: true,
-	transition: Flip,
 	hideProgressBar: true,
 });
 
@@ -27,12 +25,6 @@ interface AppProps {
 }
 
 class App extends NextApp<AppProps> {
-	static async getInitialProps({ Component, ctx }: AppContext) {
-		return {
-			pageProps: Component.getInitialProps ? await Component.getInitialProps(ctx) : {},
-		};
-	}
-
 	initialiseGA() {
 		if (process.browser) {
 			if (process.env.NODE_ENV === 'production') {
@@ -47,8 +39,9 @@ class App extends NextApp<AppProps> {
 	}
 
 	render() {
-		const { Component, pageProps, store } = this.props;
+		const { Component, store } = this.props;
 		this.initialiseGA();
+
 		return (
 			<>
 				<Head>
@@ -61,7 +54,7 @@ class App extends NextApp<AppProps> {
 					<Navbar />
 
 					<main>
-						<Component {...pageProps} />
+						<Component />
 					</main>
 				</Provider>
 			</>
